d814bea716dae0bfd55cbfb4c35abe515f767b1d
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-07 20:22:50 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(num)
+  i = num
+  while i > 0 do
+    puts "#{i} SECOND(S)!"
+    i -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(num)
+  i = num
+  while i > 0 do
+    puts "#{i} SECOND(S)!"
+    i -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
